77a99f51fd2791a8d3bc17c3c28ee429b957ce01
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-09-02 15:12:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-    $("#domValue").click(function() {
-        if (document.getElementById("csvCheck").checked == true) {
-            var str = $("#dom").val().split(',');
-        } else {
-            var str = $("#dom").val().split('\n');
+  $("#domValue").click(function() {
+    if (document.getElementById("csvCheck").checked == true) {
+      var str = $("#dom").val().split(',');
+    }
+    else {
+    var str = $("#dom").val().split('\n');
+    }
+    if (str.length > 0) {
+      $("#bulk").removeClass('hide');
+    }
+    console.log(str)
+    // alert(str);
+
+     function sleep() {
+          setTimeout(function() {  
+             console.log("waiting"); 
+          }, 240000);
+     }
+
+    for (i = 0; i < str.length; ++i) {
+      
+        $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://api.godaddy.com/v1/appraisal/' + str[i]), function(data) {
+         if (JSON.parse(data.contents).status != "OK" || JSON.parse(data.contents).status != "UNSUPPORTED_DOMAIN" || JSON.parse(data.contents).status != "MASKED_DOMAIN") {
+            sleep();
         }
-        if (str.length > 0) {
-            $("#bulk").removeClass('hide');
-        }
-        console.log(str)
-        // alert(str);
 
-        function sleep() {
-            setTimeout(function() {
-                console.log("waiting");
-            }, 240000);
-        }
+        if(JSON.parse(data.contents).domain != undefined && JSON.parse(data.contents).govalue != undefined) {
+        txt = "<tr><td>" + JSON.parse(data.contents).domain + "</td><td>" + JSON.parse(data.contents).govalue + "</td></tr>";
+        
+        $("#bulk").append(txt);
+        document.querySelector('#bulk').scrollIntoView({
+  behavior: 'smooth' 
+});
+}
+      });
 
-        for (i = 0; i < str.length; ++i) {
-
-            $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://api.godaddy.com/v1/appraisal/' + str[i]), function(data) {
-                if (JSON.parse(data.contents).status != "OK" || JSON.parse(data.contents).status != "UNSUPPORTED_DOMAIN" || JSON.parse(data.contents).status != "MASKED_DOMAIN") {
-                    sleep();
-                }
-
-                if (JSON.parse(data.contents).domain != undefined && JSON.parse(data.contents).govalue != undefined) {
-                    txt = "<tr><td>" + JSON.parse(data.contents).domain + "</td><td>" + JSON.parse(data.contents).govalue + "</td></tr>";
-
-                    $("#bulk").append(txt);
-                    document.querySelector('#bulk').scrollIntoView({
-                        behavior: 'smooth'
-                    });
-                }
-            });
-
-        }
-    });
+    }
+  });
 
 });
 
@@ -46,9 +47,7 @@ function download_csv(csv, filename) {
     var downloadLink;
 
     // CSV FILE
-    csvFile = new Blob([csv], {
-        type: "text/csv"
-    });
+    csvFile = new Blob([csv], {type: "text/csv"});
 
     // Download link
     downloadLink = document.createElement("a");
@@ -70,27 +69,26 @@ function download_csv(csv, filename) {
 }
 
 function export_table_to_csv(html, filename) {
-    var csv = [];
-    var rows = document.querySelectorAll("table tr");
-
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+	
     for (var i = 0; i < rows.length; i++) {
-        var row = [],
-            cols = rows[i].querySelectorAll("td, th");
-
-        for (var j = 0; j < cols.length; j++)
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		
+        for (var j = 0; j < cols.length; j++) 
             row.push(cols[j].innerText);
-
-        csv.push(row.join(","));
-    }
+        
+		csv.push(row.join(","));		
+	}
 
     // Download CSV
     download_csv(csv.join("\n"), filename);
 }
 
-document.getElementById('csv').addEventListener("click", function() {
+document.getElementById('csv').addEventListener("click", function () {
     var date = new Date();
     var html = document.querySelector("table").outerHTML;
-    export_table_to_csv(html, "bulk_appraisal_" + date.getTime() + ".csv");
+	export_table_to_csv(html, "bulk_appraisal_"+date.getTime()+".csv");
 });
 
 // push notification
